3ae65161b2d9fb274e8ff68f105aa8ca52810b29
Barry <160387047+Barry57@users.noreply.github.com>
2025-09-12 10:33:47 +0800

    Update deepconf-online-trans.py

--- deepconf-online-trans.py
+++ deepconf-online-trans.py
@@ -6,7 +6,7 @@ import numpy as np
 from datetime import datetime
 from vllm import LLM, SamplingParams
 from transformers import AutoTokenizer
-from helper import process_batch_results, weighted_majority_vote
+from helper_trans import process_batch_results, weighted_majority_vote
 
 MODEL_PATH = "/dbfs/FileStore/models/qwen3-1.7B-finetune-TM32/checkpoint-24975"
 MAX_TOKENS = 512
