d12be6f2d7483f9231046524b130fb22bcb87b07
Barry <160387047+Barry57@users.noreply.github.com>
2025-09-17 15:45:31 +0800

    Update deepconf-offline-trans.py

--- deepconf-offline-trans.py
+++ deepconf-offline-trans.py
@@ -12,7 +12,7 @@ from torchmetrics.text import TranslationEditRate
 # ====== 配置（offline 参数） ======
 MODEL_PATH = "/dbfs/FileStore/models/qwen3-1.7B-finetune-TM32/checkpoint-24975"
 MAX_TOKENS = 512
-TOTAL_BUDGET = 10 # 生成路径数
+TOTAL_BUDGET = 1 # 生成路径数
 WINDOW_SIZE = 3
 
 def make_token_conf_pairs(tokens, confs):
